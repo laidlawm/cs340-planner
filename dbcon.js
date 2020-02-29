bc0754cb230a3d0956394cc83c8cbdb700b5749e
hchrist2010 <christhu@oregonstate.edu>
2020-02-28 16:43:33 -0800

    Established connection and basic query example

--- dbcon.js
+++ dbcon.js
@@ -1,9 +1,34 @@
-var mysql = require('mysql');
-var pool = mysql.createPool({
-  connectionLimit : 10,
-  host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_christhu',
-  password        : '1550',
-  database        : 'cs340_christhu'
+// var mysql = require('mysql');
+// var pool = mysql.createPool({
+//   connectionLimit : 10,
+//   host            : 'classmysql.engr.oregonstate.edu',
+//   user            : 'cs340_christhu',
+//   password        : '1550',
+//   database        : 'cs340_christhu'
+// });
+// module.exports.pool = pool;
+
+const mysql = require('mysql');
+
+const con = mysql.createConnection({
+  host: 'classmysql.engr.oregonstate.edu',
+  user: 'cs340_christhu',
+  password: '1550',
+  database: 'cs340_christhu'
 });
-module.exports.pool = pool;
+con.connect((err) => {
+  if(err){
+    console.log('Error connecting to Db');
+    return;
+  }
+  console.log('Connection established');
+});
+
+con.query('SELECT * FROM Board', (err,rows) => {
+  if(err) throw err;
+  // rows.forEach( (row) => {
+  //   console.log(`${row.name} lives in ${row.city}`);
+  // });
+  console.log('Data received from Db:');
+  console.log(rows);
+});
